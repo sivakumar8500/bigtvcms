62e4d25bf4f79b01b2a3e79627de66fba93ec317
Lenovo <jogisivakumar.eee@gmail.com>
2026-08-07 18:05:40 +0530

    feat: add BigTV brand favicon icons and metadata configuration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,20 @@
 import React from 'react';
+import { Metadata } from 'next';
 import { ThemeProvider } from '@/shared/providers/ThemeProvider';
+
+export const metadata: Metadata = {
+  title: 'BIG TV CMS',
+  description: 'BIG TV Content Management System',
+  icons: {
+    icon: [
+      { url: '/favicon.ico' },
+      { url: '/favicon.png', type: 'image/png' },
+      { url: '/bigtv_logo.png', type: 'image/png' },
+    ],
+    shortcut: '/favicon.ico',
+    apple: '/bigtv_logo.png',
+  },
+};
 
 export default function RootLayout({
   children,
@@ -9,6 +24,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        {/* Favicon icons */}
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="/favicon.png" type="image/png" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" href="/bigtv_logo.png" />
+
         {/* Load Poppins Google Font for the entire application */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
